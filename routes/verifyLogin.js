fe2427cda3a0fb78582c6dec3978e20922a4a3cf
yogesh sharma <ysyogesh.sharma99@gmail.com>
2024-04-27 19:03:27 +0530

    login bug solved

--- routes/verifyLogin.js
+++ routes/verifyLogin.js
@@ -1,14 +1,13 @@
-const { jwt } = require("jsonwebtoken");
-const UserModel = require("../models/user.model");
+const  jwt  = require("jsonwebtoken");
+const UserModel = require("../models/user.model.js");
 
 const verifyLogin=async (req, res) => {
     //Authenticating user
     const response = await UserModel.find(req.body).exec();
 
     //NOw generating the jwt for the user with the email and isAdmin as payload
-
     const payload = response.length!=0 && {email:response[0].email, isAdmin:response[0].isAdmin, isUser:response[0].isUser};
-
+    
     const token = response.length!=0 && jwt.sign(payload, process.env.JWT_SECRET_ACCESS_KEY);
 
     if (response.length != 0) {
