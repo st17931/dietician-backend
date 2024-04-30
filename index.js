e820f9ea3b67e3be9e8e458fe2b28e9f849999dd
yogesh sharma <ysyogesh.sharma99@gmail.com>
2024-04-30 23:50:15 +0530

    backend updated

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = require("./app.js")
 
 async function connectDb(){
     await mongoose.connect("mongodb+srv://st17931:St17931@cluster0.qtv4gie.mongodb.net/dieticianDb?retryWrites=true&w=majority&appName=Cluster0");
-    app.listen(3333, () => {
-    console.log(`Example app listening on port ${3333}`)
+    app.listen(3000, () => {
+    console.log(`Example app listening on port ${3000}`)
   })
 }
 
