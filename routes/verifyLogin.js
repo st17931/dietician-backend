560e9058f2a04137debb3233f3a3e615a752d250
yogesh sharma <ysyogesh.sharma99@gmail.com>
2024-05-25 10:52:16 +0530

    add name in token

--- routes/verifyLogin.js
+++ routes/verifyLogin.js
@@ -4,9 +4,8 @@ const UserModel = require("../models/user.model.js");
 const verifyLogin=async (req, res) => {
     //Authenticating user
     const response = await UserModel.find(req.body).exec();
-
     //NOw generating the jwt for the user with the email and isAdmin as payload
-    const payload = response.length!=0 && {email:response[0].email, isAdmin:response[0].isAdmin, isUser:response[0].isUser};
+    const payload = response.length!=0 && {name: response[0].firstName +" "+response[0].lastName, email:response[0].email, isAdmin:response[0].isAdmin, isUser:response[0].isUser};
     
     const token = response.length!=0 && jwt.sign(payload, process.env.JWT_SECRET_ACCESS_KEY);
 
