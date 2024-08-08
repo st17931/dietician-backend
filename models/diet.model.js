a29a3938270e93a4a9760ce253154a9bd0c86612
Satyam Tripathi <st17931@gmail.com>
2024-08-08 12:42:05 +0530

    added fetch

--- models/diet.model.js
+++ models/diet.model.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
 
-const mealSchema = new mongoose.Schema([{
-    categoryName: { type: String },
-    nutritionData: { type: Object },
-    warmupData: { type: Object },
-    cardioData: { type: Object },
-    workoutData: { type: Object },
-    absData: { type: Object },
-    supplementData: { type: Object },
-    groceryData: { type: Object },
-    instructionsData: { type: Object }
-}]);
+// const mealSchema = new mongoose.Schema([{
+//     categoryName: { type: String },
+//     nutritionData: { type: Object },
+//     warmupData: { type: Object },
+//     cardioData: { type: Object },
+//     workoutData: { type: Object },
+//     absData: { type: Object },
+//     supplementData: { type: Object },
+//     groceryData: { type: Object },
+//     instructionsData: { type: Object }
+// }]);
+
+const mealSchema = new mongoose.Schema({
+    dietName:{ type : String },
+    ingredientsList:[{
+        ingredientName:{ type: String },
+        fat:{ type: String },
+        protein:{ type: String },
+        carb:{ type: String }
+    }]
+})
 
 const Meal = mongoose.model('Meal', mealSchema);
 
